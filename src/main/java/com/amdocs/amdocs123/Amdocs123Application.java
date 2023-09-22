6ec37bc120f9d3bc8d4a92a3774646285dce310d
Asia9 <shaikasiabegum12@gmail.com>
2023-09-22 09:12:51 +0530

    comit java files

---
+++ src/main/java/com/amdocs/amdocs123/Amdocs123Application.java
@@ -0,0 +1,13 @@
+package com.amdocs.amdocs123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amdocs123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amdocs123Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/amdocs/amdocs123/Amdocs123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdocs.amdocs123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Amdocs123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
